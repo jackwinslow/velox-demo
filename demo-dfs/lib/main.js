65a073bf8783e7137b3b38635d7d4a122468efe9
Justin Parsons <parsonjj@bc.edu>
2023-03-13 15:45:29 +0000

    bdfs update

--- demo-dfs/lib/main.js
+++ demo-dfs/lib/main.js
@@ -1,8 +1,14 @@
 import {Velox} from "velox-songbird"
 
-const f = new Map();
-const fc = new Map();
-const reqd = new Map();
+const files = new Map();
+const file_callbacks = new Map();
+const requested = new Map();
+const peers = new Map();
+let peer_target = 0;
+
+
+
+
 const v = new Velox("ws:139.144.30.74:80/nest", 
     {
         iceServers: [
@@ -25,6 +31,42 @@ const v = new Velox("ws:139.144.30.74:80/nest",
     }
 );
 
+v.registerMessage("PC", (m) => {
+        if (peer_target == 0) {
+            peer_target = Math.floor(m.Body/3)+1
+        }
+        if (peers.size == peer_target) {
+
+            for (const k of file_callbacks.keys()) {    
+                if (!files.has(k)) {
+                    console.log("requested resource:", k, "| Time:", Math.floor(Date.now() / 1000))
+                    requested.set(k)
+                    v.requestBlob(k, [[...peers.keys()][[Math.floor(Math.random() * peers.size)]]]).then((b) => {
+                        if (!files.has(k)) {
+                            console.log("injecting from peer:", k, "| Time:", Math.floor(Date.now() / 1000))
+                            files.set(k, b)
+                            file_callbacks.get(k)()
+                        }
+                    })
+            
+                    setTimeout( async () => {
+                        if (!files.has(k)) {
+                            const response = await fetch(`http://139.144.30.74:8080/${k}`)
+                            const nb = await response.blob()
+                            const nf = new Blob([nb], {
+                                type: nb.type,
+                            });
+                            files.set(k, nf)
+                            file_callbacks.get(k)()
+                            v.mountBlob(k, nf)
+                            console.log("injecting from server:", k, "| Time:", Math.floor(Date.now() / 1000))
+                        }
+                    }, 5000)
+                }
+            }
+        }
+
+})
 
 
 
@@ -36,8 +78,8 @@ document.addEventListener("DOMContentLoaded", () => {
         const n = i
         const path = toBlob[i].getAttribute("data-src")
         toBlob[n].removeAttribute("data-src")
-        fc.set(path, () => {
-            const furl = URL.createObjectURL(f.get(path))
+        file_callbacks.set(path, () => {
+            const furl = URL.createObjectURL(files.get(path))
             toBlob[n].setAttribute("src", furl)
         })
     }
@@ -47,33 +89,18 @@ document.addEventListener("DOMContentLoaded", () => {
     v.connect("image-demo");
 
     v.onchannelopen((peer) => {
-            for (const k of fc.keys()) {
-                if (!f.has(k)) {
-                    console.log("requested resource:", k, "| Time:", Math.floor(Date.now() / 1000))
-                    reqd.set(k)
-                    v.requestBlob(k, [peer]).then((b) => {
-                        if (!f.has(k)) {
-                            console.log("injecting from peer:", k, "| Time:", Math.floor(Date.now() / 1000))
-                            f.set(k, b)
-                            fc.get(k)()
-                        }
-                    })
-            
-                    setTimeout( async () => {
-                        if (!f.has(k)) {
-                            const response = await fetch(`http://139.144.30.74:8080/${k}`)
-                            const nb = await response.blob()
-                            const nf = new Blob([nb], {
-                                type: nb.type,
-                            });
-                            f.set(k, nf)
-                            fc.get(k)()
-                            v.mountBlob(k, nf)
-                            console.log("injecting from server:", k, "| Time:", Math.floor(Date.now() / 1000))
-                        }
-                    }, 60000)
-                }
-            }
+
+            peers.set(peer)
+
+            v.send({Type: "PC", Body:peers.size})
+
+            console.log(peers.size, peer_target)
+
         }
     )
+
+    v.onchannelclose((peer) => {
+        console.log("closed connection to:", peer)
+        peers.delete(peer)
+    })
 })
